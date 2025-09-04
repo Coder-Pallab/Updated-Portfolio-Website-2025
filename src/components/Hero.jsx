3d7a2472f60869835ddae6d8b12aa5e360354256
Coder-Pallab <duarahpallab0@gmail.com>
2025-09-04 22:59:16 +0530

    logo change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Avatar from "../assets/Pallab-non-bg.png"
+import Avatar from "../assets/Pallab-logo.png"
 import { motion } from "motion/react";
 import { Link } from "react-scroll";
 
